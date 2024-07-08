b944209e8585b65887a5b2a73d3efe5aed369375
Rafael <rafaelpradopaniago@gmail.com>
2024-07-08 10:10:18 -0300

    Inserindo o início da classe Heap

--- include/heap.hpp
+++ include/heap.hpp
@@ -1,10 +1,24 @@
 #ifndef HEAP_H
 #define HEAP_H
-
+#include "TipoItem.hpp"
+#include "TipoNo.hpp"
 class Heap{
 
-    
+    public:
 
+        Heap();
+        ~Heap();
+        void Insere(TipoItem item);
+        void InsereRecursivo(TipoNo* &p, TipoItem item);
+        TipoNo* Remove(TipoItem item);
+        TipoNo* RemoveRecursivo(TipoNo* &p, TipoItem item);
+        void ApagaRecursivo(TipoNo* &p);
+        void Limpa();
+        TipoNo* GetRaiz();
+
+    private:
+
+        TipoNo* raiz;
 };
 
 #endif
--- src/heap.cpp
+++ src/heap.cpp
@@ -1,2 +1,61 @@
 #include "heap.hpp"
 
+TipoNo* Heap::GetRaiz(){
+
+    return raiz;
+
+}
+
+void Heap::Insere(TipoItem item){
+
+    InsereRecursivo(raiz,item);
+
+}
+
+void Heap::Limpa(){
+
+    ApagaRecursivo(raiz);
+
+}
+
+Heap::~Heap(){
+
+    Limpa();
+
+}
+
+void Heap::ApagaRecursivo(TipoNo* &p){
+
+    if(p!=nullptr){
+
+        ApagaRecursivo(p->dir);
+        ApagaRecursivo(p->esq);
+        delete p;
+
+    }
+
+}
+
+TipoNo* Heap::Remove(TipoItem item){
+
+    return RemoveRecursivo(raiz,item);
+
+}
+
+Heap::Heap(){
+
+    raiz = nullptr;
+
+}
+
+void Heap::InsereRecursivo(TipoNo* &p, TipoItem item){
+
+
+
+}
+
+TipoNo* RemoveRecursivo(TipoNo* &p, TipoItem item){
+
+
+
+}
